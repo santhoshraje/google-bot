4f2ca2669f2bf4ba5dc709fca372cebe99408abb
Santhoshraj <santhoshraje95@gmail.com>
2020-05-13 13:09:44 +0800

    Added start command to bot
    Added support for basic /start command

--- bot.py
+++ bot.py
@@ -1,11 +1,33 @@
   #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 """
 Simple telegram bot framework
 """
+import logging
+
+from telegram.ext import Updater, CommandHandler
+
+# Command handler
+def start(update, context):
+    update.message.reply_text('Welcome to my Bot!')
 
 def main():
-    print('Hello World')
+    updater = Updater("TOKEN", use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Block until you press Ctrl-C or the process receives SIGINT, SIGTERM or
+    # SIGABRT. This should be used most of the time, since start_polling() is
+    # non-blocking and will stop the bot gracefully.
+    updater.idle()
 
 if __name__ == '__main__':
     main()
