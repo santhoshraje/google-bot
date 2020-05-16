723aa1bcf460c083df1424806085770c15c5cfb6
Santhoshraj <santhoshraje95@gmail.com>
2020-05-16 20:47:16 +0800

    Testing new functionality

--- rss.py
+++ rss.py
@@ -1,4 +1,5 @@
 import feedparser
+from datetime import date
 
 def getFeedData():
     data = ""
@@ -6,15 +7,40 @@ def getFeedData():
     url = 'https://trends.google.com/trends/trendingsearches/daily/rss?geo=SG'
     feed = feedparser.parse(url)
     for post in feed.entries:  
-         data += str(str(i) + '. ' + post['title'] + ' (' +  post['ht_approx_traffic'] + ' searches) \n')
+         data += str(str(i) + '. ' + 
+         post['title'] + ' (' +  
+         post['ht_approx_traffic'] + 
+         ' searches) \n')
          i+=1
     return data    
 
 def testFeed():
+    today = date.today().strftime("%d %B")
     feed = feedparser.parse('https://trends.google.com/trends/trendingsearches/daily/rss?geo=SG')
+
     for post in feed.entries: 
-        print(post['title'])
-        print(post['ht_approx_traffic'])
-        print(post['ht_news_item_title'])
+        if today in str(post['published']):
+            title = str(post['title'])
+            traffic = str(post['ht_approx_traffic'])
+            expanded_title = str(post['ht_news_item_title'])
+            snippet = str(post['ht_news_item_snippet'])
+            url = str(post['ht_news_item_url'])
+            image_url = post['ht_picture']
+            # remove unwanted characters
+            for r in (("&#39;", "'"), ("&nbsp;", "")):
+                title = title.replace(*r)
+                traffic = traffic.replace(*r)
+                expanded_title = expanded_title.replace(*r)
+                snippet = snippet.replace(*r)
+                url = url.replace(*r)
+            print(title)
+            print(traffic)
+            print(expanded_title)
+            print(snippet)
+            print(url)
+            print(image_url)
+            print('\n')
+            
+        # print(post.keys())
 
 testFeed()
