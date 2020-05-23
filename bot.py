c92cbf4d2434f7f8dc3dda33cef8c723c3c93624
Santhoshraj <santhoshraje95@gmail.com>
2020-05-23 13:13:18 +0800

    update: created bot class
    - all bot functions are now in this class
    - currently able to post rss feed data to channel or to user talking to the bot
    - no repeated posts unless bot is restarted
    - RSS feed for singapore and us

--- bot.py
+++ bot.py
@@ -1,38 +1,55 @@
-  #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from config_loader import get_config_value as load
-from rss import getFeedData as rssdata
-"""
-Simple telegram bot framework
-"""
-
 from telegram.ext import Updater, CommandHandler
+from config_loader import get_config_value as load
+from trending_searches_rss import GoogleTrendingSearch
 
-# Command handler
-def start(update, context):
-    update.message.reply_text('Welcome to my Bot!')
+class Bot:
+    def __init__(self):
+        self.token = load('BOT_TOKEN')
+        # telegram api
+        self.updater = Updater(self.token, use_context=True)
+        # Get the dispatcher to register handlers
+        self.dp = self.updater.dispatcher   
+        self.dp.add_handler(CommandHandler("start", self.start))
+        self.dp.add_handler(CommandHandler("feed", self.google_trending_sg))
+        self.dp.add_handler(CommandHandler("channel", self.google_trending_us))
+        self.posted_array = [0]
+        self.channel_id = ''
+        self.image_url = 'https://miro.medium.com/max/821/1*Fi6masemXJT3Q8YWekQCDQ.png'
 
-def rss(update, context):
-    update.message.reply_text('Fetching feed please wait')
-    update.message.reply_text(rssdata())
 
-def main():
-    updater = Updater(load('BOT_TOKEN'), use_context=True)
+    def start_bot(self):
+        print('bot active')
+        self.updater.start_polling()
+    
+    def start(self, update, context):
+        context.bot.send_message(chat_id= update.effective_chat.id, text="I'm a bot, please talk to me!") 
+    
+    def google_trending_sg(self, update, context):
+        feed_url = 'https://trends.google.com/trends/trendingsearches/daily/rss?geo=SG'
+        # get feed
+        gfeed = GoogleTrendingSearch(feed_url)
+        gfeed.get_feed_data_today()
+        for item in gfeed.feed_array:
+            if item.id not in self.posted_array:
+                self.posted_array.append(item.id)
+                context.bot.send_photo(chat_id = update.effective_chat.id, photo = self.image_url, caption = item.formatted_beta())
+                break
 
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
+    
+    def google_trending_us(self, update, context):
+        feed_url = 'https://trends.google.com/trends/trendingsearches/daily/rss?geo=US'
+        # get feed
+        gfeed = GoogleTrendingSearch(feed_url)
+        gfeed.get_feed_data_all()
+        for item in gfeed.feed_array:
+            if item.id not in self.posted_array:
+                self.posted_array.append(item.id)
+                context.bot.send_photo(chat_id = self.channel_id, photo = self.image_url, caption = item.formatted_beta_us(), disable_notification = True)
+                break
 
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("rss", rss))
 
-    # Start the Bot
-    updater.start_polling()
 
-    # Block until you press Ctrl-C or the process receives SIGINT, SIGTERM or
-    # SIGABRT. This should be used most of the time, since start_polling() is
-    # non-blocking and will stop the bot gracefully.
-    updater.idle()
+        
+    
 
-if __name__ == '__main__':
-    main()
+    
