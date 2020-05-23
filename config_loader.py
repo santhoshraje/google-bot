55faa24ccf4f2f263b4ce5dff3aa21909519af0a
Santhoshraj <santhoshraje95@gmail.com>
2020-05-23 12:14:09 +0800

    update: changed name of config file

--- config_loader.py
+++ config_loader.py
@@ -1,6 +1,6 @@
 import json
 
-def get_config_value(second, first='DEFAULT', config_name = 'bot_config.json'):
+def get_config_value(second, first='DEFAULT', config_name = 'config.json'):
     with open(config_name, 'r') as f:
         config = json.load(f)
     return config[first][second]
