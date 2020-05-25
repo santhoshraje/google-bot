ae94951374e2a901343fb94a9d36152747920664
Santhoshraj <santhoshraje95@gmail.com>
2020-05-25 20:24:06 +0800

    update: config file name is now compulsory

--- config_loader.py
+++ config_loader.py
@@ -1,6 +1,6 @@
 import json
 
-def get_config_value(second, first='DEFAULT', config_name = 'config.json'):
+def get_config_value(config_name, second, first='DEFAULT'):
     with open(config_name, 'r') as f:
         config = json.load(f)
     return config[first][second]
